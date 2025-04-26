b0752a2321dc661b882953c25305630a2a3dab81
kushacharya <kta15318@gmail.com>
2025-04-26 19:34:13 +0530

    added reduction functions

--- Tensor/Reduction.go
+++ Tensor/Reduction.go
@@ -94,3 +94,81 @@ func (t *Tensor) ArgMax() (int, int, error) {
 	return maxRow, maxCol, nil
 
 }
+
+func (t *Tensor) ArgMin() (int, int, error) {
+	if t == nil || t.Data == nil {
+		return 0, 0, errors.New("can not do ArgMin operation on null or uninitialized tensor")
+	}
+
+	minTensor := math.Inf(+1)
+	minRow := 0
+	minCol := 0
+	for i := 0; i < t.Rows; i++ {
+		for j := 0; j < t.Cols; j++ {
+			if t.Data[i][j] < minTensor {
+				minTensor = t.Data[i][j]
+				minRow = i
+				minCol = j
+			}
+		}
+	}
+	return minRow, minCol, nil
+}
+
+func (t *Tensor) Flatten() ([]float64, error) {
+	if t == nil || t.Data == nil {
+		return nil, errors.New("can not flatten a empty matrix")
+	}
+
+	result := make([]float64, t.Cols*t.Rows)
+
+	for i := 0; i < t.Rows; i++ {
+		for j := 0; j < t.Cols; j++ {
+			result[i*t.Cols+j] = t.Data[i][j]
+		}
+	}
+
+	return result, nil
+}
+
+func (a *Tensor) Equal(b *Tensor) (bool, error) {
+	if a == nil || a.Data == nil || b == nil || b.Data == nil {
+		return false, errors.New("can not compare uninitialized matrix")
+	}
+
+	if a.Rows != b.Rows || a.Cols != b.Cols {
+		return false, errors.New("matrix are not compatible for the comparison")
+	}
+
+	for i := 0; i < a.Rows; i++ {
+		for j := 0; j < a.Cols; j++ {
+			if a.Data[i][j] != b.Data[i][j] {
+				return false, nil
+			}
+		}
+	}
+
+	return true, nil
+}
+
+func (a *Tensor) AllClose(b *Tensor) (bool, error) {
+	if a == nil || a.Data == nil || b == nil || b.Data == nil {
+		return false, errors.New("can not compare uninitialized matrix")
+	}
+
+	if a.Rows != b.Rows || a.Cols != b.Cols {
+		return false, errors.New("matrix are not compatible for the comparison")
+	}
+
+	const e = 1e-6
+
+	for i := 0; i < a.Rows; i++ {
+		for j := 0; j < a.Cols; j++ {
+			if math.Abs(a.Data[i][j]-b.Data[i][j]) > e {
+				return false, nil
+			}
+		}
+	}
+
+	return true, nil
+}
